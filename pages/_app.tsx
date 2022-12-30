9d805295cfc59d73c17b77b62242f72fb356deff
Hwang Tae Young <89963995+hty0525@users.noreply.github.com>
2022-12-30 21:50:02 +0900

    Revert "Dev"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,6 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import Head from "next/head";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-const client = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <QueryClientProvider client={client}>
-      {process.env.NODE_ENV !== "production" ? (
-        <ReactQueryDevtools initialIsOpen={false} />
-      ) : null}
-      <Head>
-        <title>포켓몬!</title>
-      </Head>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />
 }
