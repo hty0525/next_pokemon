887432e907c8df8ddb2d944ea09073648dd92f92
Hwang Tae Young <hty3466@naver.com>
2023-01-27 20:03:41 +0900

    style/디테일 페이지 반응형 완료

--- components/pokemonList/pokemonDetail/PokemonDetail.tsx
+++ components/pokemonList/pokemonDetail/PokemonDetail.tsx
@@ -36,13 +36,13 @@ export default function PokemonDetail({ id }: { id: string | string[] }) {
     isImgisSuccess && isNameisSuccess && isTypeisSuccess && isClassisSuccess;
 
   return (
-    <article className="h-[calc(100vh-6rem)] flex items-center ">
+    <article className="pt-10 md:h-[calc(100vh-6rem)] flex items-center ">
       {!isSuccess ? (
         <DetailSkeleton />
       ) : (
-        <section className="w-full flex items-center m-auto h-3/4 bg-white rounded-3xl border-4 border-gray-800">
-          <ul className="flex gap-14 items-center w-full px-[5%]">
-            <li className="w-[45%]">
+        <section className="w-full flex items-center m-auto md:h-3/4 bg-white rounded-3xl border-4 border-gray-800">
+          <ul className="flex gap-14 items-center w-full flex-col md:flex-row p-[10%] md:px-[5%]">
+            <li className="md:w-[40%] w-full p-[5%]">
               <div className="relative w-full pb-[100%]">
                 <Image
                   src={imgUrl}
@@ -54,17 +54,17 @@ export default function PokemonDetail({ id }: { id: string | string[] }) {
               </div>
             </li>
             <li className="flex-1">
-              <h3 className="text-5xl mb-10 text font-extrabold">
+              <h3 className="text-4xl lg:text-5xl mb-10 text font-extrabold">
                 <p className="text-[0.4em] text-gray-400 leading-5 font-bold">
                   No.{" "}
                   {id.length < 2 ? `00${id}` : id.length < 3 ? `0${id}` : id}
                 </p>
                 {pokemonName}
               </h3>
-              <div className="mb-10 text-l">
-                <p className="text-2xl ">{pokeDesc}</p>
+              <div className="mb-10">
+                <p className="text-xl lg:text-2xl">{pokeDesc}</p>
               </div>
-              <div className="flex text-xl">
+              <div className="flex lg:text-xl">
                 <div className="text-center mr-10">
                   <p className="mb-4 text-gray-400">타입</p>
                   <div className="flex gap-3">
