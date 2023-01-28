ad9017a4a4a65cc8f4cf51addb2c2a8884872e2b
Hwang Tae Young <hty3466@naver.com>
2023-01-28 13:00:34 +0900

    nav/반응형 네비게이션

--- components/header/nav/Nav.tsx
+++ components/header/nav/Nav.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import useMediaQuery from "../../../hook/useMediaQuery";
 
 import SearchPokemon from "../searchPokemon/SearchPokemon";
 
@@ -9,9 +10,19 @@ export default function Nav({
 }: {
   setIsSearched: Dispatch<SetStateAction<boolean>>;
 }) {
+  const [isToggle, setIsToggle] = useState(true);
+
+  const isHidden = useMediaQuery("(min-width: 768px)");
+
+  useEffect(() => {
+    if (!isHidden) {
+      setIsToggle(false);
+    }
+  }, [isHidden]);
+
   return (
-    <nav className="flex items-center justify-center m-auto relative h-full max-w-[1200px]">
-      <h1 className="absolute left-0 hidden md:block">
+    <nav className="flex items-center justify-center m-auto relative h-full max-w-[1200px] overflow-hidden">
+      <h1 className="md:absolute left-0 static">
         <Link href="/">
           <Image
             src="/image/main_logo.png"
@@ -22,7 +33,22 @@ export default function Nav({
           />
         </Link>
       </h1>
-      <SearchPokemon setIsSearched={setIsSearched} />
+      <SearchPokemon setIsSearched={setIsSearched} isToggle={isToggle} />
+      {!isHidden && (
+        <button
+          className="absolute top-[50%] translate-y-[-50%] right-4 w-10 h-10 z-50"
+          onClick={() => {
+            setIsToggle(!isToggle);
+          }}
+        >
+          <Image
+            src="/image/smallMonsterBall.png"
+            alt="searchIcon"
+            width={40}
+            height={40}
+          />
+        </button>
+      )}
     </nav>
   );
 }
