f197062110f9da47a39dad71b9ddb105ab0a688d
Hwang Tae Young <hty3466@naver.com>
2023-01-30 22:46:27 +0900

    style/카드 UI번호 이름 폰트사이즈 수정

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 import Header from "./header/Header";
 import MainSlide from "./mainSlide/MainSlide";
@@ -6,6 +7,9 @@ import MainSlide from "./mainSlide/MainSlide";
 export default function Layout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const isMain = router.pathname === "/";
+  useEffect(() => {
+    history.scrollRestoration = "auto";
+  }, []);
 
   return (
     <main className="relative">
